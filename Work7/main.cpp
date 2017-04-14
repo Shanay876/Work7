55184bb360d3bfb6b617e68c77005a1f2895c39e
Shanay876 <ssaddler67@gmail.com>
2017-04-14 11:22:17 -0400

    Initial Commit

---
+++ Work7/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Work7
+//
+//  Created by Shanay Saddler on 4/14/17.
+//  Copyright © 2017 Shanay Saddler. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
